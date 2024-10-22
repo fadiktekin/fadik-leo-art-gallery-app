1de306de6262bfd5f585f917130db0c2a62a5c91
leonel-acosta <leoacosta88@gmail.com>
2024-10-22 10:49:59 +0200

    routing

--- components/ArtPiecePreview/index.js
+++ components/ArtPiecePreview/index.js
@@ -1,15 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
-import { StyledCard, StyledTitle, StyledPieceTitle, StyledArtistName } from "./ArtPiecePreview.styled";
+import {
+  StyledCard,
+  StyledTitle,
+  StyledPieceTitle,
+  StyledArtistName,
+} from "./ArtPiecePreview.styled";
 
 export default function ArtPiecePreview({ image, title, artist, slug }) {
-  return <StyledCard>
-      <Image src={image} alt={title}  width={360} height={300}></Image>
+  return (
+    <StyledCard>
+      <Image src={image} alt={title} width={360} height={300}></Image>
       <StyledTitle>
-      <Link href={`/art-pieces/${slug}`}>
-        <StyledPieceTitle>{title}</StyledPieceTitle>
-      </Link>
+        <Link href={`${slug}`}>
+          <StyledPieceTitle>{title}</StyledPieceTitle>
+        </Link>
         <StyledArtistName>{artist}</StyledArtistName>
       </StyledTitle>
     </StyledCard>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-export default function HomePage() {
+import { useRouter } from "next/router";
+import Head from "next/head";
+
+export default function SpotlightPage() {
+  const router = useRouter();
+  const { slug } = router.query;
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <>
+      <Head>
+        <title>Spotlight: {name}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>
+        <h1>{slug}</h1>
+      </div>
+    </>
   );
 }
