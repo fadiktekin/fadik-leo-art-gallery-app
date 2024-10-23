e083a574ecc378c28d3ff3b22043d5ac51d910a0
leonel-acosta <leoacosta88@gmail.com>
2024-10-23 14:48:48 +0200

    us9-color palette

--- components/Colors/index.js
+++ components/Colors/index.js
@@ -1,3 +1,23 @@
-export default function Colors() {
-  return <div>Colors</div>;
+import styles from "./colors.module.css";
+
+export default function Colors({ colors }) {
+  console.log(colors);
+  return (
+    <div className={styles.colorpalette}>
+      <h3>COLOR PALETTE</h3>
+      {colors ? (
+        colors.map((color) => {
+          return (
+            <div
+              key={color}
+              className={styles.color}
+              style={{ backgroundColor: `${color}` }}
+            ></div>
+          );
+        })
+      ) : (
+        <p>No color palette available</p>
+      )}
+    </div>
+  );
 }
