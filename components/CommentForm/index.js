88ed0a6f62725302e90e6d065a9be1cae6c3902b
leonel-acosta <leoacosta88@gmail.com>
2024-10-23 11:47:24 +0200

    us7-comment form and comment list

--- components/CommentForm/index.js
+++ components/CommentForm/index.js
@@ -1,3 +1,28 @@
+import styles from "./form.module.css";
+
 export default function CommentForm({ onSubmitComment }) {
-  return <div>CommentForm</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const comment = form.elements.comment.value;
+    onSubmitComment(comment);
+    e.target.reset();
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <legend className={styles.legend}>Add comment</legend>
+        <label htmlFor="comment">
+          <textarea
+            name="comment"
+            id="comment"
+            type="textarea"
+            className={styles.textarea}
+          ></textarea>
+        </label>
+        <button className={styles.button}>Send</button>
+      </form>
+    </>
+  );
 }
